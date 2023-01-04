580ab1cb0db3ac0e395e8dc6a85032e1dc9052e6
afonya2 <64413731+afonya2@users.noreply.github.com>
2023-01-04 18:37:28 +0100

    Added config without wallet data

---
+++ config.lua
@@ -0,0 +1,23 @@
+return {
+  ["Shop-Name"]="", --Your shop name
+  ["Description"]="", --Something greething
+  ["Owner"]="", --The name of the shop owner
+  ["Wallet-Key"]="", --Your wallet privateKey
+  ["Wallet-id"]="", --Your wallet address
+  ["Redstone_Output"]="top", --The redstone output
+  ["Chest-Id"]="minecraft:chest_", --The chest id of your network
+  ["Self-Id"]="turtle_", --The turtle id of your network
+  ["Discord-Webhook"]=false, --Enable if you want discord webhook
+  ["Discord-Webhook-URL"]="", --Your discord webhook url here
+  ["Theme"]={
+    ["Background-Color"]=0x100, --The shop's background color
+    ["Text-Color"]=0x800 --The shop's text color
+  },
+  ["Items"]={ --The items to sell
+    {
+      ["Name"]="Example", --Some friendly name
+      ["Id"]="minecraft:wheat", --The name of the item
+      ["Price"]=1 --The price / item
+    },
+  }
+}
